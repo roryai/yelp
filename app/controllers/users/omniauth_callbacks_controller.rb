da0029e716c7fcdcebf1fa7456cb4ebae714e478
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-17 18:57:48 +0000

    facebook login actually working :)

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,21 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  p "number 1"
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      p "number 2"
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      p "number 3"
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
